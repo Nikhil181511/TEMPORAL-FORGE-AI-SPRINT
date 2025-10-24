2d955d2b8c03e16238fac753dd5758266672cb12
Sakshi80506 <sakshikamble0506@gmail.com>
2025-10-24 14:34:53 +0530

    ss

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,26 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import HomePage from "./pages/HomePage";
 import DetailPage from "./pages/DetailPage";
+import MarketHarmonyDashboard from './components/MarketHarmonyDashboard';
+
 import "./App.css";
 
 function App() {
   return (
+    
+      
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/overview" element={<HomePage />} />
-        <Route path="/details/:id" element={<DetailPage />} />
+        <Route path="/details/:id" element={<MarketHarmonyDashboard />} />
+        <Route path="/dale" element={<MarketHarmonyDashboard />} />
       </Routes>
     </Router>
-import './App.css';
-import MarketHarmonyDashboard from './components/MarketHarmonyDashboard';
-
-function App() {
-  return (
-    <div className="App">
-      <MarketHarmonyDashboard />
-    </div>
   );
 }
-
 export default App;
